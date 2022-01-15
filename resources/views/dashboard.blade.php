acb8d7bf166b6712d2dc1bdffbb2eeed11756fd2
Purenatnicha0833954960 <60128438+Purenatnicha0833954960@users.noreply.github.com>
2022-01-15 23:14:28 +0700

    PureRyu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,7 @@
     </h2>
 </x-slot>
 
+<div class="py-55">
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
@@ -12,16 +13,19 @@
                 <div class="flex-auto text-2xl mb-4">Tasks Lists</div>
                 
                 <div class="flex-auto text-right mt-2">
+                    
                     <a href="/task" class="bg-blue-500 hover:bg-blue-700 text font-bold py-2 px-4 rounded">Add new Task</a>
                 </div>
             </div>
             <table class="w-full text-md rounded mb-4">
                 <thead>
+                
                 <tr class="border-b">
                     <th class="text-left p-3 px-5">Task</th>
                     <th class="text-left p-3 px-5">Actions</th>
                     <th></th>
                 </tr>
+
                 </thead>
                 <tbody>
                 @foreach(auth()->user()->tasks as $task)
@@ -43,6 +47,7 @@
             </table>
             
         </div>
+    </div>
     </div>
 </div>
 </x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,6 +28,7 @@
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">LOG IN</button>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
