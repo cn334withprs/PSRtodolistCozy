d7f14fd83e374a02600befea4a7735dc8af2920a
Purenatnicha0833954960 <60128438+Purenatnicha0833954960@users.noreply.github.com>
2022-03-17 15:54:57 +0700

    testHTTP

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -29,6 +29,27 @@ class TasksController extends Controller
     	return redirect('/dashboard'); 
     }
 
+    public function createUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);        
+        
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);        
+        
+        $user->save();        
+        
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
     public function edit(Task $task)
     {
 
