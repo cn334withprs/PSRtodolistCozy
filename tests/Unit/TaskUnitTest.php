3d1e282ae37c524eb18ae19a86786623a24fc714
6210743131 <thitiporn.tan@dome.tu.ac.th>
2022-04-20 23:36:32 +0700

    Taskunit

--- tests/Unit/TaskUnitTest.php
+++ tests/Unit/TaskUnitTest.php
@@ -8,31 +8,46 @@ use App\Models\Task;
 
 class TaskUnitTest extends TestCase
 { 
+    //check username password are the same account? if correct return success.
+    public function user_email_name(){
+        $email1 = new Task([
+            'user_email' => "user1@psr.com",
+            'name' => "Phitsinee"
 
-    public function testTaskCreation()
-    {
+        ]);
+        $this->assertEquals('Phitsinee',$email1->name);
+        $this->assertEquals('user1@psr.com',$email1->user_email);
+
+    }
+    //check email with alphabets ABCDEab and @,if yes return success.
+    public function username_alphabet(){
+        $user_email1 = new Task([
+            'user_email' => "user1@psr.com"
+
+        ]);
+        $this->assertTrue(true);
+
+    }
+    //check password more than 8 and have spacial Char
+    public function passwordWithSpacialChar(){
+        $password = new Task([
+            'password' => "!@##$$%^&**()"
+
+        ]);
+        $this->assertTrue(true);
+
+    }
+    //check email thai-language
+    public function emailWithThaiLang(){
+        $thai_email1 = new Task([
+            'thai_email' => "กขคงจ@gmail.com",
+            'condition' => "sa@gmail.com"
+        ]);
+        $this->assertNotEquals('กขคงจ@gmail.com',$thai_email1->condition);
 
     }
 
-    //Test Schema that description is correct
-    public function test_schema_description()
-    {
-
-    }
-
-    //Test Schema that User id is correct
-    public function test_schema_userID()
-    {
-
-    }
-
-    //Test Schema รองรับภาษาไทย
-
-    //Test Schema รองรับคำที่ยาวมากได้ไหม
-
-    //Test Schema null ได้ไหม
-
-
+    
 
 
 
