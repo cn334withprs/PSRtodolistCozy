1eb489a58763fe99b86827951965e4d53f0dc691
itsryuuz <94314132+itsryuuz@users.noreply.github.com>
2022-04-20 21:31:37 +0700

    test assertTrue in loginBAT

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -1,5 +1,33 @@
 <?php
 
 class LoginBAT {
-    
+    //user not register
+    public function testUserNotRegister()
+    {
+        $this->assertTrue(true);
+    }
+
+    //weird user (ex #%$%^$^%$&^$%^)
+    public function testWeirdUser()
+    {
+        $this->assertTrue(true);
+    }
+
+    //weird password (ex #%$%^$^%$&^$%^)
+    public function testWeirdPassword()
+    {
+        $this->assertTrue(true);
+    }
+
+    //none email
+    public function nonEmail()
+    {
+        $this->assertTrue(true);
+    }
+
+    //non password
+    public function nonPassword()
+    {
+        $this->assertTrue(true);
+    }
 }
