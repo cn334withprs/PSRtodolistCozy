42b6dc809053c81d162b0d28ffa7a5636aada288
6210743131 <thitiporn.tan@dome.tu.ac.th>
2022-04-20 23:08:30 +0700

    Edit

--- tests/BAT/EditTaskBAT.php
+++ tests/BAT/EditTaskBAT.php
@@ -4,25 +4,42 @@ class EditTaskBAT {
   //edit normal-Task (task = ABCDEabcde)
   public function testEditEngAlphaTask()
   {
-      $this->assertTrue(true);
+    $task1 = new Task([
+        'text' => 'ABCDEabcd',
+        'condition' => 'ABCDEabcd'
+    ]);
+    $this->assertEquals('ABCDEabcd',$task1->text);
   }
 
   //edit number Task (task = 1234567890)
   public function testEditNumberTask()
   {
-     $this->assertTrue(true);
+    $task1 = new Task([
+        'text' => '1234567890',
+        'condition' => '1234567890'
+    ]);
+    $this->assertEquals('1234567890',$task1->text);
+
   }
 
   //edit spacial-Alphabets (++๑๑๒๒๓๔๔!@#$%^&*())
-  public function testEditSpacialTask()
+  public function testEditTask()
   {
-      $this->assertTrue(true);
+    $task1 = new Task([
+        'text' => '++๑๑๒๒๓๔๔!@#$%^&*()',
+        'condition' => '++๑๑๒๒๓๔๔!@#$%^&*()'
+    ]);
+    $this->assertEquals('++๑๑๒๒๓๔๔!@#$%^&*()',$task1->text);
   }
 
   //edit non-text-task or Blank (task =   )
   public function testEditBlankTask()
   {
-      $this->assertTrue(true);
+    $task1 = new Task([
+        'text' => ' ',
+        'condition' => ' '
+    ]);
+    $this->assertEquals(' ',$task1->text);
   }
 
 
