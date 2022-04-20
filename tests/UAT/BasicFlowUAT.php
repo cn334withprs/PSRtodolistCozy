adfa3e9bfba6fae84fdb975ec660d5e742075d72
Purenatnicha0833954960 <60128438+Purenatnicha0833954960@users.noreply.github.com>
2022-04-20 22:12:05 +0700

    BasicFlowUAT

--- tests/UAT/BasicFlowUAT.php
+++ tests/UAT/BasicFlowUAT.php
@@ -2,8 +2,33 @@
 
 class BasicFlowUAT {
     //add task
+    public function test_add_task_todolist()
+    {
+        $this->assertTrue(true);
+    }
+
     //edited task
+    public function test_edit_task_todolist()
+    {
+        $this->assertTrue(true);
+    }
+
     //deleted task
+    public function test_delete_task_todolist()
+    {
+        $this->assertTrue(true);
+    }
+
     //regis
-    //log in
+    public function test_register_func()
+    {
+        $this->assertTrue(true);
+    }
+
+    //test log in func
+    public function test_login_func()
+    {
+        $this->assertTrue(true);
+    }
+
 }
