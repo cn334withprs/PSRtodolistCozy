3937da1fe67202c4eedce2e56588f5ca49cf3f19
itsryuuz <94314132+itsryuuz@users.noreply.github.com>
2022-04-20 22:52:13 +0700

    test LoginBAT

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -5,7 +5,6 @@ class LoginBAT {
     //user not register
     public function testUserNotRegister()
     {
-
         $user = new User([
             'email' => "usernotregister@mail.com",
             'password' => bcrypt("testpassword")
@@ -18,7 +17,13 @@ class LoginBAT {
     //weird user (ex #%$%^$^%$&^$%^)
     public function testWeirdUser()
     {
-        $this->assertTrue(true);
+        $user = new User([
+            'email' => "#%$%^$^%$&^$%^@mail.com",
+            'password' => bcrypt("testpassword")
+
+        ]);
+
+        $this->assertNotEquals('psr@mail.com', $user->email);
     }
 
     //weird password (ex #%$%^$^%$&^$%^)
@@ -30,7 +35,13 @@ class LoginBAT {
     //none email
     public function nonEmail()
     {
-        $this->assertTrue(true);
+        $user = new User([
+            'email' => "",
+            'password' => bcrypt("testpassword")
+
+        ]);
+
+        $this->assertNotEquals('psr@mail.com', $user->email);
     }
 
     //non password
