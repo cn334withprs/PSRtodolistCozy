ea320c9268ca2ea7e903695f05ad34d59cbecfe7
6210743131 <thitiporn.tan@dome.tu.ac.th>
2022-04-20 22:28:00 +0700

    deletetask-condition

--- tests/BAT/DeleteTaskBAT.php
+++ tests/BAT/DeleteTaskBAT.php
@@ -4,30 +4,45 @@ class DeleteTaskBAT {
      //delete normal english-alphabets (task = abcdefgABCDEFG)
      public function testDeleteNormalTask()
      {
+        $task1 = new Task([
+            'text' => 'I want to sleep'
+        ]);
          $this->assertTrue(true);
      }
 
      //delete thai-alphabets (task = กขคงจ)
      public function testDeleteThaiAlphaTask()
      {
+        $task1 = new Task([
+            'text' => 'กินข้าวเช้า'
+        ]);
          $this->assertTrue(true);
      }
 
      //delete number Task (task = 1234567890)
      public function testDeleteNumberTask()
      {
+        $task1 = new Task([
+            'text' => '1234567890'
+        ]);
         $this->assertTrue(true);
      }
  
      //delete (++๑๑๒๒๓๔๔!@#$%^&*())
      public function testDeleteWeirdTask()
      {
+        $task1 = new Task([
+            'text' => '๑๒๓๔!@#$%^&๕๖'
+        ]);
          $this->assertTrue(true);
      }
  
      //delete non-text-task or Blank (task =   )
      public function testDeleteBlankTask()
      {
+        $task1 = new Task([
+            'text' => ' '
+        ]);
          $this->assertTrue(true);
      }
 
