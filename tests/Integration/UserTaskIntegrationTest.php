fc610bbebd20eced6deb977121acb296f4f428e1
Purenatnicha0833954960 <60128438+Purenatnicha0833954960@users.noreply.github.com>
2022-04-20 16:18:36 +0700

    userTaskIntegration

--- tests/Integration/UserTaskIntegrationTest.php
+++ tests/Integration/UserTaskIntegrationTest.php
@@ -1,7 +1,33 @@
 <?php
 
 class UserTaskIntegrationTest 
-{ 
+{
+    //User don't have test
+    public function user_have_no_test()
+    {
+        $this->assertTrue(true);
+    }
+    
+    //User have multi test
+    public function user_have_multi_test()
+    {
+        $this->assertTrue(true);
+    }
+
+    //Task deleted by user
+    public function user_task_deleted_by_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    //Task create by user
+    public function user_task_create_by_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    
+
 
 
 }
