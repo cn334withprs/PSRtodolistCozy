c786fb7d2ec78efcd284c0d26998b2958801caf2
6210743131 <thitiporn.tan@dome.tu.ac.th>
2022-04-20 22:03:00 +0700

    Edit&Delete

--- tests/BAT/DeleteTaskBAT.php
+++ tests/BAT/DeleteTaskBAT.php
@@ -1,5 +1,36 @@
 <?php
 
 class DeleteTaskBAT {
-    
+     //delete normal english-alphabets (task = abcdefgABCDEFG)
+     public function testDeleteNormalTask()
+     {
+         $this->assertTrue(true);
+     }
+
+     //delete thai-alphabets (task = กขคงจ)
+     public function testDeleteThaiAlphaTask()
+     {
+         $this->assertTrue(true);
+     }
+
+     //delete number Task (task = 1234567890)
+     public function testDeleteNumberTask()
+     {
+        $this->assertTrue(true);
+     }
+ 
+     //delete (++๑๑๒๒๓๔๔!@#$%^&*())
+     public function testDeleteWeirdTask()
+     {
+         $this->assertTrue(true);
+     }
+ 
+     //delete non-text-task or Blank (task =   )
+     public function testDeleteBlankTask()
+     {
+         $this->assertTrue(true);
+     }
+
+
+ 
 }
--- tests/BAT/EditTaskBAT.php
+++ tests/BAT/EditTaskBAT.php
@@ -1,5 +1,31 @@
 <?php
 
 class EditTaskBAT {
-    
+  //edit normal-Task (task = ABCDEabcde)
+  public function testEditEngAlphaTask()
+  {
+      $this->assertTrue(true);
+  }
+
+  //edit number Task (task = 1234567890)
+  public function testEditNumberTask()
+  {
+     $this->assertTrue(true);
+  }
+
+  //edit spacial-Alphabets (++๑๑๒๒๓๔๔!@#$%^&*())
+  public function testEditSpacialTask()
+  {
+      $this->assertTrue(true);
+  }
+
+  //edit non-text-task or Blank (task =   )
+  public function testEditBlankTask()
+  {
+      $this->assertTrue(true);
+  }
+
+
+
+
 }
